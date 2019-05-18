82b0c4bb50273f8782994ebe0c464495a34c9640
miiiyan <miiiyan.2525@gmail.com>
2019-05-18 12:06:47 +0900

    5月18日の授業

---
+++ FifteenPuzzleProject/Assets/mainScene/Scripts/Control/CubeCtrl.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCtrl : MonoBehaviour
+{
+    private int panelNumber = 0;
+    public int PanelNumber
+    {
+        get { return panelNumber; }
+        set { panelNumber = value; }
+    }
+
+    private Vector3 chengeCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    Debug.Log(panelNumber);
+                    RaycastHit emptyHit = new RaycastHit();
+                    Ray upRay = new Ray(hit.transform.position, hit.transform.forward);
+                    Ray downRay = new Ray(hit.transform.position, -hit.transform.forward);
+                    Ray rightRay = new Ray(hit.transform.position, hit.transform.right);
+                    Ray leftRay = new Ray(hit.transform.position, -hit.transform.right);
+                    Debug.DrawRay(downRay.origin, downRay.direction, Color.red, 20.0f); 
+
+                    if (Physics.Raycast(upRay.origin, upRay.direction, out emptyHit, Mathf.Infinity))
+                    {
+                        if(emptyHit.collider.tag == "Empty15")
+                        {
+                            chengeCube = transform.position;
+                            transform.position = emptyHit.collider.transform.position;
+                            emptyHit.collider.transform.position = chengeCube;
+                        }
+                        else
+                        {
+                            Debug.Log("失敗");
+                        }
+                    }
+                    else if (Physics.Raycast(downRay.origin, downRay.direction, out emptyHit, Mathf.Infinity))
+                    {
+                        if (emptyHit.collider.tag == "Empty15")
+                        {
+                            Debug.Log("Hit");
+                            chengeCube = transform.position;
+                            transform.position = emptyHit.collider.transform.position;
+                            emptyHit.collider.transform.position = chengeCube;
+                        }
+                    }
+                    else if (Physics.Raycast(rightRay.origin, rightRay.direction, out emptyHit, Mathf.Infinity))
+                    {
+                        if (emptyHit.collider.tag == "Empty15")
+                        {
+                            chengeCube = transform.position;
+                            transform.position = emptyHit.collider.transform.position;
+                            emptyHit.collider.transform.position = chengeCube;
+                        }
+                    }
+                    else if (Physics.Raycast(leftRay.origin, leftRay.direction, out emptyHit, Mathf.Infinity))
+                    {
+                        if (emptyHit.collider.tag == "Empty15")
+                        {
+                            emptyHit.collider.transform.position = chengeCube;
+
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
--- FifteenPuzzleProject/Assets/mainScene/Scripts/Control/PuzzleCtrl.cs
+++ FifteenPuzzleProject/Assets/mainScene/Scripts/Control/PuzzleCtrl.cs
@@ -1,54 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PuzzleCtrl : MonoBehaviour
 {
-    private GameObject cube;
-    private GameObject empty15;
+    [SerializeField]
+    private List<GameObject> cubesList = new List<GameObject>();
+
+    private List<int> cubeNumsList = new List<int>();
 
     private Vector3 cubePos;
-    private float cubePosX;
-    private float cubePosZ;
+    private Vector3 emptyPos;
 
-    private int[] cubeNmb;
+    private int chengeNum;
 
     // Start is called before the first frame update
     void Start()
     {
-        cubePos = cube.transform.position;
+        cubeNumsList = Enumerable.Range(0, 16).ToList();
+        foreach (var obj in cubeNumsList.Select((item, index) => new { item, index }))
+        {
+            cubesList[obj.item].GetComponent<CubeCtrl>().PanelNumber = obj.item;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Ray ray = new Ray();
-            RaycastHit hit = new RaycastHit();
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue
-            if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
-            {
-
-            }
-        }
-        
     }
 
     public void PosChenge (int x, int z)
     {
         int pos1 = z * 4 + x;
         int pos2 = -1;
-        if (x > 0 && cubeNmb[pos1 - 1] == 15) pos2 = pos1 - 1;
-        if (x < 3 && cubeNmb[pos1 + 1] == 15) pos2 = pos1 + 1;
-        if (z > 0 && cubeNmb[pos1 - 1] == 15) pos2 = pos1 - 4;
-        if (z < 3 && cubeNmb[pos1 + 1] == 15) pos2 = pos1 + 4;
+        if (x > 0 && cubeNumsList[pos1 - 1] == 15) pos2 = pos1 - 1;
+        if (x < 3 && cubeNumsList[pos1 + 1] == 15) pos2 = pos1 + 1;
+        if (z > 0 && cubeNumsList[pos1 - 1] == 15) pos2 = pos1 - 4;
+        if (z < 3 && cubeNumsList[pos1 + 1] == 15) pos2 = pos1 + 4;
         if (pos2 != -1)
         {
-            cubePos = new Vector3(cubePosX, cubePos.y, cubePosZ);
-            cube.transform.position = cubePos;
+            cubePos = new Vector3(emptyPos.x, cubePos.y, cubePos.z);
+            cubesList[pos1].transform.position = cubePos;
         }
     }
 }
